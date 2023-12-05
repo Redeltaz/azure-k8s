5d56bcc1b1e2000b067061bba30c489e8a1a246c
Redeltaz <campistron.lucas@gmail.com>
2023-12-05 16:25:04 +0100

    finish project

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -4,6 +4,18 @@ output "public_ip" {
   description = "Value for the public IP address used to access the app"
 }
 
+output "acr_name" {
+  value = azurerm_container_registry.container_registry.name
+
+  description = "Name of the Container Registry"
+}
+
+output "acr_url" {
+  value = azurerm_container_registry.container_registry.login_server
+
+  description = "URL of the Container Registry"
+}
+
 resource "local_file" "kubeconfig" {
   depends_on = [azurerm_kubernetes_cluster.kubernetes_cluster]
   filename   = "../kubernetes/kubeconfig"
